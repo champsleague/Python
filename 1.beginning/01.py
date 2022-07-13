2b32b744dae349b4a8d6b9d79015348147fa57cb
youkeongjun <youkeongjun@naver.com>
2022-07-13 14:33:26 +0900

    first commit

---
+++ 1.beginning/01.py
@@ -0,0 +1,14 @@
+print("game start")
+number = 10
+exact_num = input("from 1 to 100")
+print(exact_num)
+
+guess = int(exact_num)
+
+if guess == number : 
+    print("correct")
+else :
+    print("wrong")
+
+
+
